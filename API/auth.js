b8bc54a8899f7dbce62b3256f9237decbf270563
Andrew Culkin <andrewsculkin@gmail.com>
2021-10-25 17:45:23 -0400

    Simplify auth API returns

--- API/auth.js
+++ API/auth.js
@@ -4,20 +4,18 @@ import apiRoutes from './apiRoutes'
 
 ///test API routes for now
 export const auth = {
-	getToken: (userToGetToken) => {
-		return axios.post(apiRoutes.auth.getToken, userToGetToken)
-	},
-	getUser: (optionalToken) => {
-		return optionalToken
+	getToken: (userToGetToken) =>
+		axios.post(apiRoutes.auth.getToken, userToGetToken),
+	getUser: (optionalToken) =>
+		optionalToken
 			? axios.get(apiRoutes.auth.getUser, {
 					headers: { Authorization: `Token ${optionalToken}` }
 			  })
-			: axios.get(apiRoutes.auth.getUser)
-	},
-	createUser: (newUser) => {
-		return axios.post(apiRoutes.auth.signup, newUser)
-	},
+			: axios.get(apiRoutes.auth.getUser),
+	createUser: (newUser) => axios.post(apiRoutes.auth.createUser, newUser),
+	editUser: (userDetailsToEdit) =>
+		axios.patch(apiRoutes.auth.getUser, userDetailsToEdit),
 	logout: () => {
 		return Promise.resolve()
-	},
+	}
 }
